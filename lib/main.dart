5422fee6878aee72b81db5b62baed95c8bccb268
Omar Faruk <omarfaruk4543@gmail.com>
2022-09-12 12:26:56 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:help_app/ui/homepage.dart';
+
+void main() => runApp(new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MainPage(),
+    ));
---
+++ lib/ui/homepage.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('GET HELP', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),),
+        backgroundColor: Colors.orange,
+        actions: [
+          IconButton(onPressed:  () {}, icon: Icon(Icons.account_circle_rounded))
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Color.fromARGB(255, 230, 230, 230),
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.inbox), label: "Inbox"),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home")
+        ],
+      ),
+      body: Column(
+        children: [
+          Container(
+
+          ),
+        ],
+      ),
+    );
+  }
+}
